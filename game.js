fb3f0565af3f61559c3304736fec57d51369f8a7
Alixe Jolidon <alixe.jolidon@bbz-cfp.ch>
2022-12-16 14:03:33 +0100

    kleine änderung

--- game.js
+++ game.js
@@ -5,31 +5,27 @@ const spieler = document.querySelector(".player");
 spieler.style.left = "0px";
 spieler.style.bottom = "0px";
 let timer = new Timer(55);
-var wasser = new Audio('sound/soundhintergrund.mp3')
-
+var wasser = new Audio("sound/soundhintergrund.mp3");
 
 // Damit man auf eine andere Seite kommt
 function start() {
   window.location.href = "./game.html";
-  }
-  
-  // Kollision
+}
+
+// Kollision
 
 function collision() {
   let steine = document.querySelectorAll(".stein");
-  
+
   if (anyCollision(spieler, steine)) {
     window.location.href = "./gameover.html";
     return;
   }
-
-
 }
 
 // Anker die herunterkommen
 
 function stein() {
-  
   if (timer.ready()) {
     let h = document.createElement("div");
     h.classList.add("stein");
@@ -41,22 +37,21 @@ function stein() {
     let h = document.createElement("div");
     h.classList.add("stein");
     h.style.top = "0px";
-    h.style.left = "180px";
+    h.style.left = "210px";
     spielfeld.appendChild(h);
   }
   if (timer.ready()) {
     let h = document.createElement("div");
     h.classList.add("stein");
     h.style.top = "80px";
-    h.style.left = "280px";
+    h.style.left = "420px";
     spielfeld.appendChild(h);
   }
 
-
   let steine = document.querySelectorAll(".stein");
   for (let stein of steine) {
-    stein.style.top = parseInt(stein.style.top) + 3+ "px";
-    if (parseInt(stein.style.top) > 200) {
+    stein.style.top = parseInt(stein.style.top) + 3 + "px";
+    if (parseInt(stein.style.top) > 1200) {
       stein.parentNode.removeChild(stein);
     }
   }
@@ -72,7 +67,7 @@ function versuch() {
 
 // Alle funktionen ausführen
 function loop() {
-  if (keyboard(39) && parseInt(spieler.style.left) <450) {
+  if (keyboard(39) && parseInt(spieler.style.left) < 430) {
     spieler.style.left = parseInt(spieler.style.left) + 5 + "px";
   }
   if (keyboard(37) && parseInt(spieler.style.left) > 0) {
